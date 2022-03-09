ed24c1a4b30eade6aeadcf169be033a1048a8428
bielb1b2 <gabrielvt.souza@Hotmail.com>
2022-03-09 19:02:36 -0300

    Todos os testes corrigidos

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,12 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const existsUser = this.usersRepository.findByEmail(email);
+    if(existsUser){
+      throw new Error("Mensagem do erro");
+    }
+    const user = this.usersRepository.create({ name, email });
+    return user;
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,14 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if(!user || user.admin === false){
+      throw new Error("Mensagem de erro");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
